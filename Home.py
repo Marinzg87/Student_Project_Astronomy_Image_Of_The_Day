863bffd429be0a1c1495280c67f6503d9d1a90ee
Marcin Bogusz <bogusz.marcin@icloud.com>
2024-02-26 07:21:51 +0100

    Get the image url. Download the file.

--- Home.py
+++ Home.py
@@ -3,13 +3,26 @@ import requests
 
 # API parameters
 api_key = "gamrBaDSgHEIgNLg5BjcQ42bFqKzpceDPsB8F0fQ"
-url = "https://api.nasa.gov/planetary/apod" \
-       f"apiKey={api_key}"
+url = "https://api.nasa.gov/planetary/apod?" \
+       f"api_key={api_key}&" \
+       "date=2024-02-25"
 
 # Make a request
 request = requests.get(url)
 
+# Get a dictionary with the data
+content = request.json()
 
-st.title("App to show the astronomy image of the day")
+# Get an image url and download it
+image_url = content["url"]
+response = requests.get(image_url)
+with open("image.jpg", "wb") as file:
+       file.write(response.content)
+
+# Get the description
+description = content["explanation"]
+print(description)
+
+# st.title("App to show the astronomy image of the day")
 # st.image()
-st.write("Description")
+# st.write("Description")
